739a0c2b78e17d806e79297b0e165b314271236e
Jonatan Torino <jonatan.torino@hotmail.com>
2024-12-13 13:15:35 +0000

    CRT 3-Creacion de Clases Request/Response

---
+++ CmmSDK/src/DevAxDefaultOrderType/CommerceRuntime/Messages/DevAxTrnLGRetailFunctionalityProfileRequest.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Dynamics.Commerce.Runtime.Messages;
+
+namespace DevAxDefaultOrderType.CommerceRuntime.Messages
+{
+    public class DevAxTrnLGRetailFunctionalityProfileRequest : Request
+    {
+        public DevAxTrnLGRetailFunctionalityProfileRequest()
+        {
+        }
+    }
+}
---
+++ CmmSDK/src/DevAxDefaultOrderType/CommerceRuntime/Messages/DevAxTrnLGRetailFunctionalityProfileResponse.cs
@@ -0,0 +1,15 @@
+﻿using DevAxDefaultOrderType.CommerceRuntime.Entities;
+using Microsoft.Dynamics.Commerce.Runtime.Messages;
+
+namespace DevAxDefaultOrderType.CommerceRuntime.Messages
+{
+    public class DevAxTrnLGRetailFunctionalityProfileResponse : Response
+    {
+        public TrnLGRetailFunctionalityProfile ProfileEntity { get; }
+
+        public DevAxTrnLGRetailFunctionalityProfileResponse(TrnLGRetailFunctionalityProfile profileEntity)
+        {
+            this.ProfileEntity = profileEntity;
+        }
+    }
+}
