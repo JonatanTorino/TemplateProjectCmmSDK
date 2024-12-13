33b6fb2fabddaa2f0dfad307dfe26ab6ee51d941
Jonatan Torino <jonatan.torino@hotmail.com>
2024-12-13 13:12:08 +0000

    CRT 2-Creacion de la Entidad que mapea los campos con la base de datos

---
+++ CmmSDK/src/DevAxDefaultOrderType/CommerceRuntime/Entities/TrnLGRetailFunctionalityProfile.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Dynamics.Commerce.Runtime.ComponentModel.DataAnnotations;
+using Microsoft.Dynamics.Commerce.Runtime.DataModel;
+using System.Runtime.Serialization;
+using KeyAttribute = System.ComponentModel.DataAnnotations.KeyAttribute;
+
+namespace DevAxDefaultOrderType.CommerceRuntime.Entities
+{
+    public class TrnLGRetailFunctionalityProfile : CommerceEntity
+    {
+        #region Propiedades
+
+        [Key]
+        [DataMember]
+        [Column(nameof(ProfileId))]
+        public string ProfileId
+        {
+            get { return (string)this[nameof(ProfileId)]; }
+            set { this[nameof(ProfileId)] = value; }
+        }
+
+        [DataMember]
+        [Column(nameof(TrnLGIsQuotation))]
+        public int TrnLGIsQuotation
+        {
+            get { return (int)this[nameof(TrnLGIsQuotation)]; }
+            set { this[nameof(TrnLGIsQuotation)] = value; }
+        }
+
+        [DataMember]
+        [Column(nameof(RECID))]
+        public long RECID
+        {
+            get { return (long)this[nameof(RECID)]; }
+            set { this[nameof(RECID)] = value; }
+        }
+
+        #endregion Propiedades
+
+        public TrnLGRetailFunctionalityProfile() : base(TRNLGRETAILFUNCTIONALITYPROFILE)
+        {
+        }
+
+        #region Constantes
+
+        private const string TRNLGRETAILFUNCTIONALITYPROFILE = "TRNLGRETAILFUNCTIONALITYPROFILE";
+
+        #endregion Constantes
+    }
+}
